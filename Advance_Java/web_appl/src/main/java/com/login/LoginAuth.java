435a08832e1aec7addb52338d5db12790ab8f19e
sanikashelake49 <shelakesanika49@gmail.com>
2026-05-23 19:58:56 +0530

    Update LoginAuth.java

--- Advance_Java/web_appl/src/main/java/com/login/LoginAuth.java
+++ Advance_Java/web_appl/src/main/java/com/login/LoginAuth.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.PrintWriter; 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
