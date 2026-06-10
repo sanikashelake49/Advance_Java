d239af5a4fa54c4b69c0d5838793140c1a405a21
sanikashelake49 <shelakesanika49@gmail.com>
2026-06-10 23:56:37 +0530

    Update DemoMain.java

--- Advance_Java/Hibernate/Hibernate_Demo/src/com/demo/DemoMain.java
+++ Advance_Java/Hibernate/Hibernate_Demo/src/com/demo/DemoMain.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
+   
 public class DemoMain {
 
 	@SuppressWarnings("deprecation")
